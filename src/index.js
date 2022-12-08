9d5f6e6ecac02fba44735b03219770340c37d5d4
Fabiana Hasu <fabi.hasu@gmail.com>
2022-12-08 20:10:20 -0300

    Versión con isValid

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import validator from './validator.js';
 
 window.funcionValidar = function funcionValidar(){
     const numerotarjeta = document.getElementById("creditCardNumber").value
-    validator.validate(numerotarjeta)
+    validator.isValid(numerotarjeta)
 }
 
--- src/validator.js
+++ src/validator.js
@@ -1,25 +1,34 @@
 const validator = {
   // ...
-  validate(numerotarjeta) {
-    let num = []; //Declaración de una variable = un arreglo vacío
-    num = Array.from(String(numerotarjeta), Number);  // Variable = transformando el input de string para número em un arreglo
-    let meuArrayInvertido = num.reverse(); // La variable meuArrayInvertido = recibe la función reversa en la variable num
-    for(let i=0;i < meuArrayInvertido.length;i++){ // repetición (declaração de una let i, que comienza en el índice=0, y termina en el largo del arreglo  y hace un incremento de índice más uno)
-      
-      if (i % 2 === 0) { // si... la posição es par (se o índice dividido por 2  tiver o resto idêntico a 0)
-         meuArrayInvertido[i] = meuArrayInvertido[i] * 2; // Meuarrayinvertido con los indices pares[i], con todas os ìndices pares, multiplicar * 2
-        
-         if (meuArrayInvertido[i]>=10) { //se em alguma posição os elementos do meu arreglo for maior ou igual a 10
-          let digitos = meuArrayInvertido[i].toString().split('') //declaração de uma let digitos= vai transformar meu arreglo de números [i] só os que forem >=10 em string, e aplico a função de separador de caracteres[1,6]
-          let sumadedigitos = parseInt(digitos[0]) + parseInt(digitos[1])//declaração de uma let = vai receber a função de  
-          meuArrayInvertido[i] = sumadedigitos
+  isValid(numerotarjeta) {
+    let num = []; 
+    num = Array.from(String(numerotarjeta), Number);  
+    let meuArrayInvertido = num.reverse(); 
+    for(let i=0;i < meuArrayInvertido.length;i++){ 
+      if (i % 2 !== 0) { 
+         meuArrayInvertido[i] = meuArrayInvertido[i] * 2; 
+         if (meuArrayInvertido[i]>=10) { 
+          let digitos = meuArrayInvertido[i].toString().split('') 
+          let sumadedigitos = parseInt(digitos[0]) + parseInt(digitos[1])
+          meuArrayInvertido[i] = sumadedigitos;
          }
-
-        
       } 
     }
+    
+    let total = 0;
+    for(let i = 0; i <meuArrayInvertido.length; i++){
+      total = total + meuArrayInvertido[i]
+    }
 
-    alert(meuArrayInvertido)
+    //alert(meuArrayInvertido)
+   // alert(total)
+
+    if(total % 10 == 0) {
+      alert("La tarjeta es válida")
+    } else{
+      alert("la tarjeta es Inválida")
+    }
+
   }
 };
 
