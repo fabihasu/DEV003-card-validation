29f995fea233e91c879a2a78134abcca4edb7b09
Fabiana Hasu <fabi.hasu@gmail.com>
2022-12-13 22:05:51 -0300

    Versión final

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 import validator from './validator.js';
 
-window.funcionValidar = function funcionValidar(){
-    const numerotarjeta = document.getElementById("creditCardNumber").value
-    validator.isValid(numerotarjeta)
+document.getElementById("botonvalidar").addEventListener("click", funcionValidar)
+document.getElementById("botonvalidar").disabled = true
+document.getElementById("creditCardNumber").addEventListener("keyup", funcionhabilitar)
+
+function funcionhabilitar(){
+  if(document.getElementById("creditCardNumber").value.length > 0 ) {
+    document.getElementById("botonvalidar").disabled = false 
+  } 
+  if (document.getElementById("creditCardNumber").value.length === 0 ) {
+    document.getElementById("botonvalidar").disabled = true
+  }
+}
+
+function funcionValidar(){
+  const numerotarjeta = document.getElementById("creditCardNumber").value
+ 
+  const mask = validator.maskify(numerotarjeta) // llamo funcion maskify que está en el objeto validator y mascaro el valor del input
+  document.getElementById("creditCardNumber").value = mask //Asigno al input el valor de mask 
+
+  const tarjetavalida = validator.isValid(numerotarjeta)
+  if (tarjetavalida === true) {
+    // alert ("La tarjeta es válida")
+    document.getElementById("mensajevalidacion").innerHTML ="La tarjeta es válida"
+  } else {
+    // alert ("La tarjeta es inválida")
+    document.getElementById("mensajevalidacion").innerHTML ="La tarjeta no es válida"
+
+  }
 }
 
--- src/validator.js
+++ src/validator.js
@@ -3,15 +3,15 @@ const validator = {
   isValid(numerotarjeta) {
     let num = []; 
     num = Array.from(String(numerotarjeta), Number);  
-    let meuArrayInvertido = num.reverse(); 
+    const meuArrayInvertido = num.reverse(); 
     for(let i=0;i < meuArrayInvertido.length;i++){ 
       if (i % 2 !== 0) { 
-         meuArrayInvertido[i] = meuArrayInvertido[i] * 2; 
-         if (meuArrayInvertido[i]>=10) { 
-          let digitos = meuArrayInvertido[i].toString().split('') 
-          let sumadedigitos = parseInt(digitos[0]) + parseInt(digitos[1])
+        meuArrayInvertido[i] = meuArrayInvertido[i] * 2; 
+        if (meuArrayInvertido[i]>=10) { 
+          const digitos = meuArrayInvertido[i].toString().split('') 
+          const sumadedigitos = parseInt(digitos[0]) + parseInt(digitos[1])
           meuArrayInvertido[i] = sumadedigitos;
-         }
+        }
       } 
     }
     
@@ -20,14 +20,31 @@ const validator = {
       total = total + meuArrayInvertido[i]
     }
 
-    //alert(meuArrayInvertido)
-   // alert(total)
-
-    if(total % 10 == 0) {
-      alert("La tarjeta es válida")
+    if(total % 10 === 0) {
+      return true
     } else{
-      alert("la tarjeta es Inválida")
+      return false
     }
+  },
+
+  maskify(numerotarjeta) {
+    const largodenumerodetarjeta = numerotarjeta.length // guardo el tamanho de mi numero de tarjeta       
+    let gatitos = "" // creo variable tipo string vacia
+    if (largodenumerodetarjeta > 4) { //si tengo mas de 4 digitos, entonces enmascaro
+      for(let i=0; i < largodenumerodetarjeta - 4; i++) { // recorro mis numeros hasta el largo menos 4,
+        gatitos = gatitos + "#" // guardo un gatito por cada repeticion del for
+      }
+      const ultimos4digitos = numerotarjeta.slice(-4) // guardo los ultimos 4 digitos
+      const tarjetaenmascarada = gatitos + ultimos4digitos // concateno los dos string, los gatitos y los ultimos 4 digitos
+      return tarjetaenmascarada // retorno mi numero enmascarado
+    } else {
+      return numerotarjeta // retorno mi mismo numero cuando tengo 4 o menos digitos
+    }
+    //criar uma variavel que mostre somente os últimos quatro caracteres . substring
+    //criar outra variavel que mostre o resto dos números,substituindo pelo símbolo que quero mostrar
+    //concatenar as variaveis
+    //alert que mostre isso....primeira versão
+
 
   }
 };
